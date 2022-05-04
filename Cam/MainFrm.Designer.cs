ccca38285da72b1748cc6e34b52419ade926c9aa
Labasse <labasse@gmail.com>
2022-05-04 16:06:51 +0200

    Ajoutez des fichiers projet.

---
+++ Cam/MainFrm.Designer.cs
@@ -0,0 +1,65 @@
+﻿namespace Cam
+{
+    partial class MainFrm
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.videoSource = new AForge.Controls.VideoSourcePlayer();
+            this.SuspendLayout();
+            // 
+            // videoSource
+            // 
+            this.videoSource.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.videoSource.Location = new System.Drawing.Point(0, 0);
+            this.videoSource.Name = "videoSource";
+            this.videoSource.Size = new System.Drawing.Size(304, 201);
+            this.videoSource.TabIndex = 0;
+            this.videoSource.Text = "videoSourcePlayer1";
+            this.videoSource.VideoSource = null;
+            this.videoSource.MouseClick += new System.Windows.Forms.MouseEventHandler(this.videoSource_MouseClick);
+            // 
+            // MainFrm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(304, 201);
+            this.Controls.Add(this.videoSource);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.Name = "MainFrm";
+            this.Text = "Cam";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.MainFrm_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private AForge.Controls.VideoSourcePlayer videoSource;
+    }
+}
+
---
+++ Cam/MainFrm.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using AForge.Video.DirectShow;
+
+namespace Cam
+{
+    public partial class MainFrm : Form
+    {
+        private FilterInfoCollection devices;
+        private int current = -1;
+        private List<VideoCaptureDevice> captureDevices = new List<VideoCaptureDevice>();
+        public MainFrm()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnClosed(EventArgs e)
+        {
+            base.OnClosed(e);
+            foreach(VideoCaptureDevice device in captureDevices)
+            {
+                device.SignalToStop();
+            }
+        }
+
+        private void MainFrm_Load(object sender, EventArgs e)
+        {
+            devices = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            Change();
+        }
+
+        private void Change()
+        {
+            videoSource.VideoSource?.Stop();
+            videoSource.VideoSource?.WaitForStop();
+            if (devices.Count > 1)
+            {
+                current = (current + 1) % devices.Count;
+                if(captureDevices.Count <= current)
+                {
+                    captureDevices.Add(new VideoCaptureDevice(devices[current].MonikerString));
+                }
+                videoSource.VideoSource = captureDevices[current];
+                videoSource.VideoSource.Start();
+            }
+        }
+
+        private void videoSource_MouseClick(object sender, MouseEventArgs e)
+        {
+            if(e.Button.HasFlag(MouseButtons.Right))
+            {
+                Change();
+            }
+            else if(e.Button.HasFlag(MouseButtons.Left))
+            {
+                if(captureDevices[current].IsRunning)
+                {
+                    videoSource.VideoSource.Stop();
+                }
+                else
+                {
+                    videoSource.VideoSource.Start();
+                }
+            }
+            else if(e.Button.HasFlag(MouseButtons.Middle) && captureDevices[current].CheckIfCrossbarAvailable())
+            {
+                captureDevices[current].DisplayCrossbarPropertyPage(Handle);                
+            }
+            UpdateText();
+        }
+
+        private void UpdateText()
+        {
+            Text = $"Cam ({devices[current].Name})" + (captureDevices[current].IsRunning ? " ⦿" : "");
+        }
+    }
+}
